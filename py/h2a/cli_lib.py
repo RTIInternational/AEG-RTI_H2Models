82d01dfa4367f4ebfaae1f173b3b3d2ce04a9755
Roman Ruiz-Esparza <2654709+rjruizes@users.noreply.github.com>
2023-06-30 12:45:23 -0400

    mv data transform to lib, automate data in js

--- py/h2a/cli_lib.py
+++ py/h2a/cli_lib.py
@@ -29,3 +29,69 @@ def write_output(results, input):
     # Write results to out/ as JSON with filename = input filename
     with open(os.path.join("out", input), "w") as outfile:
         json.dump(results, outfile)
+
+def transform_for_graph1(input_files, results_list):
+    # Quick and dirty script to transform the output of the H2A model into a format that can be used by the graphing library
+    original_filenames = [
+        "default-autothermal-reforming-natural-gas-with-cc.json"
+        "default-smr-natural-gas-with-cc.json"
+        "default-pem-electrolysis.json"
+        "default-smr-natural-gas-no-cc.json"
+        "default-solid-oxide-electrolysis.json"
+    ]
+    filename_lookup = {
+        "default-autothermal-reforming-natural-gas-with-cc.json": "ATR with CCS",
+        "default-smr-natural-gas-with-cc.json": "SMR with CCS",
+        "default-pem-electrolysis.json": "PEM Electrolysis",
+        "default-smr-natural-gas-no-cc.json": "SMR without CCS",
+        "default-solid-oxide-electrolysis.json": "Solid Oxide Electrolysis"
+    }
+
+    breakdown_original_keys = [
+        "dollars_per_kg_h2_capital_related_costs",
+        "dollars_per_kg_h2_decommissioning_costs",
+        "dollars_per_kg_h2_fixed_cost",
+        "dollars_per_kg_h2_total_feedstock_cost",
+        "dollars_per_kg_h2_other_raw_material_cost",
+        "dollars_per_kg_h2_variable_cost",
+    ]
+    key_lookup = {
+        "dollars_per_kg_h2_capital_related_costs": "Capital Costs",
+        "dollars_per_kg_h2_decommissioning_costs": "Decommissioning Costs",
+        "dollars_per_kg_h2_fixed_cost": "Fixed Costs",
+        "dollars_per_kg_h2_total_feedstock_cost": "Total Feedstock Cost",
+        "dollars_per_kg_h2_other_raw_material_cost": "Other Raw Material Costs",
+        "dollars_per_kg_h2_variable_cost": "Variable Costs",
+    }
+
+    cost_breakdown = {
+        "Capital Costs": [],
+        "Decommissioning Costs": [],
+        "Fixed Costs": [],
+        "Total Feedstock Cost": [],
+        "Other Raw Material Costs": [],
+        "Variable Costs": [],
+    }
+
+    categories = []
+    for i, input in enumerate(input_files):
+        results = results_list[i]
+        categories.append(filename_lookup[input])
+        for key in breakdown_original_keys:
+            if key in results:
+                cost_breakdown[key_lookup[key]].append(results[key])
+            else:
+                print(f"Warning: {key} not found in {input}")
+
+    cost_breakdown_array = []
+    for key in cost_breakdown:
+        cost_breakdown_array.append(
+            {
+                "name": key,
+                "data": cost_breakdown[key],
+            }
+        )
+    
+    print(cost_breakdown_array)
+    print(categories)
+    return cost_breakdown_array, categories
--- py/run.py
+++ py/run.py
@@ -3,7 +3,7 @@ import typer
 from typing import List
 
 from h2a.util import root_dir
-from h2a.cli_lib import prep, write_output
+from h2a.cli_lib import prep, write_output, transform_for_graph1
 from h2a.read_input import read_inputs_json
 from h2a.formulas import calculate
 
@@ -20,70 +20,19 @@ def main(input_files: List[str], clean: bool = False):
             if filename != "input.schema.json":
                 input_files.append(filename)
 
-    original_filenames = [
-        "default-autothermal-reforming-natural-gas-with-cc.json"
-        "default-smr-natural-gas-with-cc.json"
-        "default-pem-electrolysis.json"
-        "default-smr-natural-gas-no-cc.json"
-        "default-solid-oxide-electrolysis.json"
-    ]
-    filename_lookup = {
-        "default-autothermal-reforming-natural-gas-with-cc.json": "ATR with CCS",
-        "default-smr-natural-gas-with-cc.json": "SMR with CCS",
-        "default-pem-electrolysis.json": "PEM Electrolysis",
-        "default-smr-natural-gas-no-cc.json": "SMR without CCS",
-        "default-solid-oxide-electrolysis.json": "Solid Oxide Electrolysis"
-    }
-
-    breakdown_original_keys = [
-        "dollars_per_kg_h2_capital_related_costs",
-        "dollars_per_kg_h2_decommissioning_costs",
-        "dollars_per_kg_h2_fixed_cost",
-        "dollars_per_kg_h2_total_feedstock_cost",
-        "dollars_per_kg_h2_other_raw_material_cost",
-        "dollars_per_kg_h2_variable_cost",
-    ]
-    key_lookup = {
-        "dollars_per_kg_h2_capital_related_costs": "Capital Costs",
-        "dollars_per_kg_h2_decommissioning_costs": "Decommissioning Costs",
-        "dollars_per_kg_h2_fixed_cost": "Fixed Costs",
-        "dollars_per_kg_h2_total_feedstock_cost": "Total Feedstock Cost",
-        "dollars_per_kg_h2_other_raw_material_cost": "Other Raw Material Costs",
-        "dollars_per_kg_h2_variable_cost": "Variable Costs",
-    }
-
-    cost_breakdown = {
-        "Capital Costs": [],
-        "Decommissioning Costs": [],
-        "Fixed Costs": [],
-        "Total Feedstock Cost": [],
-        "Other Raw Material Costs": [],
-        "Variable Costs": [],
-    }
-    categories = []
+    results_list = []
     for input in input_files:
         results = run_model(input)
         write_output(results, input)
 
-        # Chart info
-        categories.append(filename_lookup[input])
-        for key in breakdown_original_keys:
-            if key in results:
-                cost_breakdown[key_lookup[key]].append(results[key])
-            else:
-                print(f"Warning: {key} not found in {input}")
+        results_list.append(results)
 
-    cost_breakdown_array = []
-    for key in cost_breakdown:
-        cost_breakdown_array.append(
-            {
-                "name": key,
-                "data": cost_breakdown[key],
-            }
-        )
-    
-    print(cost_breakdown_array)
-    print(categories)
+    series, categories = transform_for_graph1(input_files, results_list)
+    # Replace var SERIES_DATA=[] in index.html
+    with open(os.path.join(root_dir, "index-data.js"), "w") as f:
+        f.write(f"window.SERIES_DATA={series};\n")
+        f.write(f"window.CATEGORIES={categories};\n")
+
 
 if __name__ == "__main__":
     typer.run(main)
