150e8e0a5fed5d2cf12905c345fa7ccf3577a69f
Roman Ruiz-Esparza <2654709+rjruizes@users.noreply.github.com>
2023-06-30 10:04:56 -0400

    run.R

--- R/run.R
+++ R/run.R
@@ -11,10 +11,14 @@ import::from("read_input.R", read_default_inputs_json, .directory = here("h2a"))
 # Import the H2A model
 import::from("formulas.R", calculate, .directory = here("h2a"))
 
-# Loop over files
-for (json_filename in args) {
+run <- function(json_filename) {
     user_input <- read_default_inputs_json(json_filename)
 
     # Run the model
     results <- calculate(user_input)
+}
+
+# Loop over files
+for (json_filename in args) {
+    run(json_filename)
 }
