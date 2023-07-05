05df6a283a0f74e8ec70dfc4cc40a591a4cbcfc9
Shane Weisberg <sweisberg@rti.org>
2023-07-05 18:09:20 -0400

    ggplot

---
+++ R/h2a/output_processing.R
@@ -0,0 +1,193 @@
+json_to_df = function(output_list, model_name) {
+  values = names(output_list)
+  df_full = data.frame("model" = c(model_name))
+  for (i in 1:length(output_list)) {
+    if (typeof(output_list[[i]]) == "list") {
+      if (values[i] == "total_process_emissions_kg_per_kg_h2") {
+        columns = data.frame("CO2_process_emissions_kg_per_kg_h2" = c(output_list[[i]]$CO2), 
+                             "CH4_process_emissions_kg_per_kg_h2" = c(output_list[[i]]$CH4),
+                             "N2O_process_emissions_kg_per_kg_h2" = c(output_list[[i]]$N2O))
+        df_full = cbind(df_full,columns)
+      } else if (values[i] == "total_upstream_emissions_kg_per_kg_h2") {
+        x = 43
+        columns = data.frame("CO2_upstream_emissions_kg_per_kg_h2" = c(output_list[[i]]$CO2), 
+                             "CH4_upstream_emissions_kg_per_kg_h2" = c(output_list[[i]]$CH4),
+                             "N2O_upstream_emissions_kg_per_kg_h2" = c(output_list[[i]]$N2O))
+        df_full = cbind(df_full,columns)
+      }
+    } else if (typeof(output_list[[i]]) %in% c("integer", "double") & length(output_list[[i]]) == 1) {
+      #row = data.frame("variable" = c(values[i]), "value" = c(output_list[[i]]))
+      column = data.frame("temp" = c(output_list[[i]]))
+      colnames(column) = c(values[i])
+      df_full = cbind(df_full,column)
+    } else if (values[i] == "total_upstream_emissions_kg_per_kg_h2") {
+      x = 44
+      columns = data.frame("CO2_upstream_emissions_kg_per_kg_h2" = c(output_list[[i]][[1]]), 
+                           "CH4_upstream_emissions_kg_per_kg_h2" = c(output_list[[i]][[2]]),
+                           "N2O_upstream_emissions_kg_per_kg_h2" = c(output_list[[i]][[3]]))
+      df_full = cbind(df_full,columns)
+    } else {
+      print(typeof(output_list[[i]]))
+    }
+    
+    #print(values[i])
+    #print(output_list[[i]])
+    #print("---")
+  }
+  return(df_full)
+}
+
+t = json_to_df(results$list,'test')
+
+cost_map = data.frame("long" = c(
+  'dollars_per_kg_h2_capital_related_costs',
+  'dollars_per_kg_h2_fixed_cost',
+  'electricity_cost_per_kg_h2',
+  'natural_gas_cost_per_kg_h2',
+  'dollars_per_kg_h2_variable_cost',
+  'dollars_per_kg_h2_other_raw_material_cost',
+  'dollars_per_kg_h2_decommissioning_costs'
+), "cost_segment" = c(
+  'Capital',
+  'Fixed',
+  'Electricity',
+  'Natural Gas',
+  'Variable',
+  'Other',
+  'Other'
+))
+
+cost_barplot = function(results_filename) {
+  columns = c(
+    'model',
+    'dollars_per_kg_h2_capital_related_costs',
+    'dollars_per_kg_h2_fixed_cost',
+    'electricity_cost_per_kg_h2',
+    'natural_gas_cost_per_kg_h2',
+    'dollars_per_kg_h2_variable_cost',
+    'dollars_per_kg_h2_other_raw_material_cost',
+    'dollars_per_kg_h2_decommissioning_costs'
+  )
+  df = read.csv(paste0("./output/",results_filename))
+  df = df %>% select(columns) %>%
+    pivot_longer(cols = 2:8, names_to = "long", values_to = "value") %>%
+    left_join(cost_map, by = "long") %>%
+    group_by(model, cost_segment) %>%
+    summarize(value = sum(value)) %>%
+    mutate(model = str_replace(model,".json","")) %>%
+    mutate(model = str_replace(model,"default-","")) %>%
+    mutate(model = str_replace(model,"-","\n")) %>%
+    mutate(cost_segment = factor(cost_segment, 
+                                 levels = c("Other", "Natural Gas", "Electricity", "Variable", "Fixed", "Capital")))
+  
+  sums = df %>% group_by(model) %>% summarize(value = sum(value))
+  
+  p = df %>% ggplot() +
+    geom_bar(aes(x = model, y = value, fill = cost_segment), stat = "identity") +
+    geom_text(aes(x = model, y = value, group = cost_segment, label = round(value,2)),
+              hjust = 0.5, position = position_stack(vjust = 0.5),
+              color = ifelse(df$cost_segment %in% c("Fixed", "Variable", "Electricity", "Natural Gas"), "white", "black"),
+              alpha = ifelse(df$value < 0.05,0,1)) +
+    geom_text(data = sums, aes(x = model, y = value, label = round(value,2)), vjust = -0.4, fontface = "bold") +
+    labs(y = "Cost ($/kg H2)", title = "H2 Cost Comparison", fill = "Cost") +
+    scale_fill_manual(values = c("Capital" = "#DAEDF4", "Electricity" = "black", "Fixed" = "#00008B", "Natural Gas" = "#008B8B", "Other" = "gray", "Variable" = "#9F0000")) +
+    scale_y_continuous(expand = expansion(add = c(0,.2))) +
+    theme_classic() + 
+    theme(axis.ticks = element_blank(), plot.title = element_text(hjust = 0.5),
+          axis.title.x = element_blank())
+              
+  ggsave(filename = paste0("./output/plots/cost.png"), plot = p, width = 7, height = 5, units = "in")
+  return(p)
+}
+
+emissions_barplot = function(results_filename) {
+  columns = c(
+    'model',
+    'CO2_process_emissions_kg_per_kg_h2',
+    'CO2_Capture_Efficiency'
+  )
+  
+  df = read.csv(paste0("./output/",results_filename))
+  df = df %>% select(columns) %>%
+    mutate('Captured' = CO2_process_emissions_kg_per_kg_h2 * CO2_Capture_Efficiency,
+           'Released' = CO2_process_emissions_kg_per_kg_h2 * (1 - CO2_Capture_Efficiency)) %>%
+    select(c(model, Captured, Released)) %>%
+    pivot_longer(cols = 2:3, names_to = "emissions", values_to = "value") %>%
+    mutate(model = str_replace(model,".json","")) %>%
+    mutate(model = str_replace(model,"default-","")) %>%
+    mutate(model = str_replace(model,"-","\n"))
+  
+  sums = df %>% group_by(model) %>% summarize(value = sum(value, na.rm = TRUE))
+  
+  p = df %>% ggplot() +
+    geom_bar(aes(x = model, y = value, fill = emissions), stat = "identity") +
+    geom_text(aes(x = model, y = value, group = emissions, label = round(value,2)), 
+              hjust = 0.5, position = position_stack(vjust = 0.5),
+              color = ifelse(df$emissions == "Released", "white", "black"),
+              alpha = ifelse(df$value == 0,0,1)) +
+    geom_text(data = sums, aes(x = model, y = value, label = round(value,2)), vjust = -0.4, fontface = "bold") +
+    labs(y = "CO2 Emissions (kg CO2 /kg H2)", title = "H2 Emissions Comparison", fill = "Emissions") +
+    scale_y_continuous(expand = expansion(add = c(0,.5))) +
+    scale_fill_manual(values = c("Released" = "#00008B", "Captured" = "#DAEDF4")) +
+    theme_classic() + 
+    theme(axis.ticks = element_blank(), plot.title = element_text(hjust = 0.5),
+          axis.title.x = element_blank())
+  
+  
+  ggsave(filename = paste0("./output/plots/emissions.png"), plot = p, width = 7, height = 5, units = "in")
+  return(p)
+}
+
+lifecycle_barplot = function(results_filename) {
+  columns = c(
+    'model',
+    'CO2_process_emissions_kg_per_kg_h2',
+    'CO2_upstream_emissions_kg_per_kg_h2',
+    'CO2_Capture_Efficiency'
+  )
+  
+  df = read.csv(paste0("./output/",results_filename))
+  df = df %>% select(columns) %>%
+    mutate('Direct' = CO2_process_emissions_kg_per_kg_h2 * (1 - CO2_Capture_Efficiency)) %>%
+    rename("Upstream" = "CO2_upstream_emissions_kg_per_kg_h2") %>%
+    select(c(model, Direct, Upstream)) %>%
+    pivot_longer(cols = 2:3, names_to = "emissions", values_to = "value") %>%
+    mutate(model = str_replace(model,".json","")) %>%
+    mutate(model = str_replace(model,"default-","")) %>%
+    mutate(model = str_replace(model,"-","\n")) %>%
+    mutate(emissions = factor(emissions, levels = c("Upstream", "Direct")))
+  
+  sums = df %>% group_by(model) %>% summarize(value = sum(value, na.rm = TRUE))
+  
+  p = df %>% ggplot() +
+    geom_bar(aes(x = model, y = value, fill = emissions), stat = "identity") +
+    geom_text(aes(x = model, y = value, group = emissions, label = round(value,2)), 
+              hjust = 0.5, position = position_stack(vjust = 0.5),
+              color = ifelse(df$emissions == "Direct", "white", "black"),
+              alpha = ifelse(df$value == 0,0,1)) +
+    geom_text(data = sums, aes(x = model, y = value, label = round(value,2)), vjust = -0.4, fontface = "bold") +
+    scale_fill_manual(values = c("Direct" = "#00008B", "Upstream" = "#DAEDF4")) +
+    labs(y = "CO2 Emissions (kg CO2 /kg H2)", title = "H2 Emissions Comparison", fill = "Emissions") +
+    scale_y_continuous(expand = expansion(add = c(0,.5))) +
+    theme_classic() + 
+    theme(axis.ticks = element_blank(), plot.title = element_text(hjust = 0.5),
+          axis.title.x = element_blank())
+  
+  ggsave(filename = paste0("./output/plots/lifecycle.png"), plot = p, width = 7, height = 5, units = "in")
+  return(p)
+}
+
+make_plots = function(results_filename) {
+  p = cost_barplot(results_filename)
+  print(p)
+  p = emissions_barplot(results_filename)
+  print(p)
+  p = lifecycle_barplot(results_filename)
+  print(p)
+}
+
+
+p = cost_barplot("test.csv")
+p = emissions_barplot("test.csv")
+p = lifecycle_barplot("test.csv")
+print(p)
--- R/run.R
+++ R/run.R
@@ -6,25 +6,54 @@ import::from(here, here)
 # Parse CLI args as a list of files
 args <- commandArgs(trailingOnly = TRUE)
 
-#args = c('default-smr-natural-gas-no-cc.json') # runs
+# args = c('default-smr-natural-gas-no-cc.json') # runs
 # args = c('default-smr-natural-gas-with-cc.json') # runs
-args = c('default-pem-electrolysis.json') # does not run
+# args = c('default-pem-electrolysis.json') # does not run
 # args = c('default-solid-oxide-electrolysis.json') # does not run
 # args = c('default-autothermal-reforming-natural-gas-with-cc.json') # runs
+args = c(
+  'default-smr-natural-gas-no-cc.json', 
+  'default-smr-natural-gas-with-cc.json',
+  'default-autothermal-reforming-natural-gas-with-cc.json'
+)
+output_filename = "sample_results.csv" # should include ".csv"
 
 # Import the user input
 import::from("read_input.R", read_default_inputs_json, .directory = here("h2a"))
 # Import the H2A model
 import::from("formulas.R", calculate, .directory = here("h2a"))
+# Import output processing
+import::from("output_processing.R", json_to_df, .directory = here("h2a"))
+import::from("output_processing.R", cost_barplot, .directory = here("h2a"))
+import::from("output_processing.R", emissions_barplot, .directory = here("h2a"))
+import::from("output_processing.R", lifecycle_barplot, .directory = here("h2a"))
+import::from("output_processing.R", make_plots, .directory = here("h2a"))
+
 
 run <- function(json_filename) {
     user_input <- read_default_inputs_json(json_filename)
 
     # Run the model
-    results <- calculate(user_input)
+    results_list = calculate(user_input)
+    results_json = toJSON(results_list)
+    write_json(results_json,paste0("./Output/",json_filename))
+    
+    return(list(
+      "list" = results_list,
+      "json" = results_json
+    ))
 }
 
 # Loop over files
+results_df = data.frame()
 for (json_filename in args) {
-    run(json_filename)
+    results = run(json_filename)
+    
+    df_row = json_to_df(results$list, json_filename)
+    results_df = bind_rows(results_df, df_row)
+    write.csv(results_df, paste0("./output/",output_filename), row.names = FALSE)
 }
+
+make_plots(output_filename)
+
+
